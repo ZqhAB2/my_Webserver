0cd6344c4a60a40d7bebe1a55dce81aabdd290b9
ZqhAB2 <96478295+ZqhAB2@users.noreply.github.com>
2024-01-27 11:38:49 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,31 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+
+#include "./lock/locker.h"
+#include "./threadpool/threadpool.h"
+#include "./timer/lst_timer.h"
+#include "./http/http_conn.h"
+#include "./log/log.h"
+#include "./CGImysql/sql_connection_pool.h"
+
+#define MAX_FD 65536           //最大文件描述符
+#define MAX_EVENT_NUMBER 10000 //最大事件数
+#define TIMESLOT 5             //最小超时单位
+
+#define SYNLOG  //同步写日志
+//#define ASYNLOG //异步写日志
+
+//#define listenfdET //边缘触发非阻塞
+#define listenfdLT //水平触发阻塞
+
 //这三个函数在http_conn.cpp中定义，改变链接属性
 extern int addfd(int epollfd, int fd, bool one_shot);
 extern int remove(int epollfd, int fd);
